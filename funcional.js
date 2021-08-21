1aa07e6f77f55ac0fe31c2e12a7abf40490e7204
camilaru15 <mcrueda69848@umanizales.edu.co>
2021-08-21 18:01:49 -0500

    formulario

--- funcional.js
+++ funcional.js
@@ -1,11 +1,39 @@
 
-function validar(){
-    var input_instituto = document.getElementById("input_instituto");
-    if(input_instituto.value == ""){
-      document.getElementById("respuestas").innerHTML = '<div class="alert alert-danger" role="alert">'+
-      'Ingrese instituto'+
-      '</div>';
-    }else{
-      document.getElementById("respuestas").innerHTML = '<div class="alert alert-success" role="alert">Se guardo correctamente</div>';
-    }
-  }
+
+$(document).ready(function() {
+  //obtenemos el valor de los input
+  
+  $('#adicionar').click(function() {
+    var _año = document.getElementById("año").value;
+    var _instituto = document.getElementById("instituto").value;
+    var _lugar = document.getElementById("lugar").value;
+    var _culmino = document.getElementById("culmino").value;
+
+    var i = 1; //contador para asignar id al boton que borrara la fila
+    var fila = '<tr id="row' + i + '"><td>' + _año + '</td><td>' + _instituto + '</td><td>' + _lugar+ '</td><td>' + _culmino+ '</td><td>';
+   // <button type="button" name="remove" id="' + i + '" class="btn btn-danger btn_remove">Quitar</button></td></tr>'; //esto seria lo que contendria la fila
+  
+    i++;
+  
+    $('#mytable tr:first').after(fila);
+      $("#adicionados").text(""); //esta instruccion limpia el div adicioandos para que no se vayan acumulando
+      var nFilas = $("#mytable tr").length;
+      $("#adicionados").append(nFilas - 1);
+      //le resto 1 para no contar la fila del header
+      document.getElementById("instituto").value ="";
+      document.getElementById("lugar").value = "";
+      document.getElementById("culmino").value = "";
+      document.getElementById("año").value = "";
+      document.getElementById("año").focus();
+    });
+  $(document).on('click', '.btn_remove', function() {
+    var button_id = $(this).attr("id");
+      //cuando da click obtenemos el id del boton
+      $('#row' + button_id + '').remove(); //borra la fila
+      //limpia el para que vuelva a contar las filas de la tabla
+      $("#adicionados").text("");
+      var nFilas = $("#mytable tr").length;
+      $("#adicionados").append(nFilas - 1);
+    });
+  });
+
